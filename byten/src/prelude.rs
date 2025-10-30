1f34abf7ea51b71cb7236387b7d02d06d29d44bf
Muhammet Ali AKBAY <ali.akbay@spaghetti-labs.com>
2025-10-30 12:45:20 +0100

    EncoderToVec prelude for codecs

--- byten/src/prelude.rs
+++ byten/src/prelude.rs
@@ -1,4 +1,4 @@
-use crate::{Encode, EncodeError, Measure};
+use crate::{Encode, EncodeError, Encoder, Measure, Measurer};
 
 pub trait EncodeToVec {
   fn encode_to_vec(&self) -> Result<Vec<u8>, EncodeError>;
@@ -10,6 +10,22 @@ impl<T: Encode + Measure> EncodeToVec for T {
         let mut vec = vec![0u8; size];
         let mut offset = 0;
         self.encode(&mut vec, &mut offset)?;
+        Ok(vec)
+    }
+}
+
+pub trait EncoderToVec {
+    type Decoded;
+    fn encode_to_vec(&self, decoded: &Self::Decoded) -> Result<Vec<u8>, EncodeError>;
+}
+
+impl<Decoded, C: Encoder<Decoded=Decoded> + Measurer<Decoded=Decoded>> EncoderToVec for C {
+    type Decoded = Decoded;
+    fn encode_to_vec(&self, decoded: &Self::Decoded) -> Result<Vec<u8>, EncodeError> {
+        let size = self.measure(decoded);
+        let mut vec = vec![0u8; size];
+        let mut offset = 0;
+        self.encode(decoded, &mut vec, &mut offset)?;
         Ok(vec)
     }
 }
